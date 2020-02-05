d107f01bd80d4f28c7a9e3a5ac10bcb6209b224b
reaperofchaos <tokugawa111@gmail.com>
2020-02-05 14:14:33 -0500

    Updates

---
+++ precache-manifest.7abdf96f367dbf986afd530c14090f41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e611e84bf89b30689349c72f02f6ce4",
+    "url": "/githubReacTest/index.html"
+  },
+  {
+    "revision": "9703d55d46ac72f13f24",
+    "url": "/githubReacTest/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e4d3885c29603a968642",
+    "url": "/githubReacTest/static/js/2.72592f31.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/githubReacTest/static/js/2.72592f31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9703d55d46ac72f13f24",
+    "url": "/githubReacTest/static/js/main.60633138.chunk.js"
+  },
+  {
+    "revision": "449d34af85730088d272",
+    "url": "/githubReacTest/static/js/runtime-main.a1f98e49.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/githubReacTest/static/media/logo.5d5d9eef.svg"
+  }
+]);
